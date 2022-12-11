ca18d59c887efeb3ce7a6ce5d3168bb6503f6025
bodwood <bodiewood@hotmail.com>
2022-12-11 12:34:57 -0800

    add a new Postcard.cshtml file which grabs information from a form and returns it within the Postcard.cshtml file

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.controller
 {
@@ -11,6 +12,24 @@ namespace FriendLetter.controller
     public string Goodbye() { return "Goodbye friend."; } //Goodbye() represents a route
 
     [Route("/")]
-    public ActionResult Letter() { return View(); } //ActionResult handles redering views. View() is a built-in method from .Mvc namepace
+    public ActionResult Letter()
+    {
+      LetterVariable myLetterVariable = new LetterVariable(); //new instance of LetterVariable
+      myLetterVariable.Recipient = "Bodie"; //use LetterVariable instance to set name of recipient to Bodie
+      myLetterVariable.Sender = "Kadysha";  //use LetterVariable instance to set name of sender to Kadysha
+      return View(myLetterVariable);  //return the view of the new instance 
+    } //ActionResult handles redering views. View() is a built-in method from .Mvc namepace
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender) //take in recipient and sender from form
+    {
+      LetterVariable myLetterVariable = new LetterVariable(); //new instance of LetterVariable
+      myLetterVariable.Recipient = recipient; //set recipitent property to recipient from parameter
+      myLetterVariable.Sender = sender; //set sender property to sender from parameter
+      return View(myLetterVariable);  //return instance
+    }
   }
 }
